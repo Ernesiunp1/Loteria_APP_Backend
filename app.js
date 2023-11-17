b3e92a62d5e4052927203a4954942e53315d7b5e
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-11-16 19:57:33 -0500

    cors2

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const conection = require("./database/conexion")
 const cors = require("cors")
 const fileUpload = require('express-fileupload')
 
+
+
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
-
-
-const PORT = process.env.PORT || 3000
-
-const app = express()
 
 conection()
 
