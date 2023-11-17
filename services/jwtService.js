e314afa38e58974a627737855af14ac3fde24b11
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-11-16 21:14:07 -0500

    hora token

--- services/jwtService.js
+++ services/jwtService.js
@@ -9,7 +9,7 @@ const token = async ( data )=>{
         id: data._id,       
         email: data.email,            
         iat: moment().unix(),
-        exp: moment().add(5 , "hours").unix()
+        exp: moment().add(1 , "mounth").unix()
     }
 
 
