a3ca81f8b8b3cf869875718ad686de197a2be3d7
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-11-16 20:17:33 -0500

    cors4

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors())
+
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -20,7 +23,6 @@ app.use((req, res, next) => {
 
 conection()
 
-// app.use(cors())
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
