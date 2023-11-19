5461ad73f8af07644f178c5a9a785d89d06c96cd
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-11-19 15:46:45 -0500

    problemas de cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', ['*', "https://sorteo-app-f6bb865852e1.herokuapp.com/"]);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
