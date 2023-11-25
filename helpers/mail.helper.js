9f52d41e20dca01930ccb082b91730bcc547171c
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-11-24 21:37:03 -0500

    correo duvan

--- helpers/mail.helper.js
+++ helpers/mail.helper.js
@@ -7,7 +7,9 @@ const {google} = require('googleapis');
 
 const sendMail = (payload) =>{
 
-    const { boletas, name, email, phone, message, total} =  payload
+    console.log("Entrando al helpper");
+
+      const { boletas, name, email, phone, message, total} =  payload
 
 
     function createTable(boletas) {
@@ -61,11 +63,10 @@ const sendMail = (payload) =>{
         
     `
 
-    const CLIENT_ID ="533437542492-cec47jqpe9fieeb4qi1h2blha665i4q4.apps.googleusercontent.com"
-    const CLIENT_SERCRET="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"
-    const REDIRECT_URL ="https://developers.google.com/oauthplayground" 
-    const REFRESH_TOKEN="1//04qbwoNzcIaNwCgYIARAAGAQSNwF-L9Irvas_ZNrcpY1zq-przX2P-ny1nehwf8ACLk4tnbTPGMpKhxQ0GDG5YWpU7aUMsy3iM8Q"
-
+    const CLIENT_ID ="719889471463-aqjtjd784ovnrrst73oe1oo95har6f21.apps.googleusercontent.com"
+    const CLIENT_SERCRET="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"
+    const REDIRECT_URL ="https://developers.google.com/oauthplayground"   
+    const REFRESH_TOKEN="1//04qg61yp1rpvbCgYIARAAGAQSNwF-L9Iro3QQx_aIzf57QuVJTn_PZFWGLDDq7Cht0RCj-i4LPfNSXvmVpI2PjBbS9DHdg44smDM"
     const oAuth2Client=new google.auth.OAuth2(CLIENT_ID, CLIENT_SERCRET, REDIRECT_URL)
 
     oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
@@ -79,7 +80,7 @@ const sendMail = (payload) =>{
                 service: "gmail",
                 auth: {
                     type: "OAuth2",
-                    user:"vivas.ernesto@gmail.com",
+                    user:"dseventos23@gmail.com",
                     clientId: CLIENT_ID,
                     clientSecret: CLIENT_SERCRET,
                     refreshToken: REFRESH_TOKEN,
@@ -88,47 +89,46 @@ const sendMail = (payload) =>{
             });
 
             const mailOptions = {
+                
                 from: "DS EVENTOS <vivas.ernesto@gmail.com>",
                 to:`${email}`,
                 subject:"DS Eventos",
-                html:`
-    <h1> Tus Boletas han sido reservadas con éxito</h1>
-    <h1> Debes pagar Inmediatamente y confirmar el pago.</h1>
-    
-    
-    <h3> Estos son tus datos: </h3> 
-        <ul>
-            <li> name: ${name}       </li>
-            <li> email: ${email}     </li>
-            <li> celular: ${phone}   </li>
-            
-            
-        </ul>
+                                html:`
+                    <h1> Tus Boletas han sido reservadas con éxito</h1>
+                    <h1> Debes pagar Inmediatamente y confirmar el pago.</h1>
+                    
+                    
+                    <h3> Estos son tus datos: </h3> 
+                        <ul>
+                            <li> name: ${name}       </li>
+                            <li> email: ${email}     </li>
+                            <li> celular: ${phone}   </li>
+                            
+                            
+                        </ul>
 
-    <h1>1.- Total a pagar : ${total}  </h1>
+                    <h1>1.- Total a pagar : ${total}  </h1>
 
-    <h1>2.- Copia la cuenta Nequi aqui: <link> 312-747-2160 </link>   </h1>
-    
-    <h1>3.- Entra a  Nequi aquí:</h1>
+                    <h1>2.- Copia la cuenta Nequi aquí:  3127472160 </h1>
+                    
+                    <h1>2.- Entra a tu banco </h1>
 
-      <link>https://transacciones.nequi.com/bdigital/login.jsp</link> 
+                    transfiere a la cuenta nequi:  3127472160
 
-    <h1>4.- Envía tu foto del pago aquí: </h1>
+                    <h1>4.- Envía la foto del pago y sigue las instrucciones aquí: </h1>
 
-     https://wa.me/573127472160/?text=Agrega%20la%20imagen%20de%20tu%20pago%20aqui%20en%20unos%20momentos%20confirmaremos%20el%20mismo
+                    <p> <a href="https://wa.me/573127472160/?text=Agrega%20la%20imagen%20de%20tu%20pago%20aqui%20con%20tus%20datos%20personales%20(nombre%20completo,%20número,%20de%20identificación,%20y%20correo%20)%20y%20en%20unos%20momentos%20confirmaremos%20el%20pago" > Click aquí para confirmar tu pago en Whatsapp ...</a> </p>
+
+                    <h1>6.- Listo campeon ya terminaste!  </h1>
+
+                    <h1>Contacto Whatsapp</h1>
+
+                        <li><link> 312-747-2160</link> </li>
+
+                        <p> mensaje: ${message} <p>
 
 
-
-    <h1>Contacto Whatsapp</h1>
-
-        <li><link> 312-747-2160</link> </li>
-
-        <p> mensaje: ${message} <p>
-
-
-        ${createTable(boletas)}
-        
-    `
+                        ${createTable(boletas)}    `
             };
 
            const result =  await transporter.sendMail(mailOptions)
